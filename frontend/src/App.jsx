ab9d6c97cdce8161694984168b1389337f957e86
caowens <caowens555@yahoo.com>
2022-11-02 13:23:47 -0500

    Connect front end to node/mongo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [posts, setPosts] = useState([{
-    content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo iste quia cum architecto autem porro modi consequuntur provident, quidem adipisci nobis eius deserunt aspernatur inventore!'
-  }],
-    [{
-      content: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia'
-    }]);
+  const [posts, setPosts] = useState([]);
+  const [post, setPost] = useState("");
+
+
+  const getPostsFromDB = () => {
+    fetch('http://localhost:3001/posts')
+      .then(res => res.json())
+      .then(data => {
+        console.log('posts:', data);
+        setPosts(data.data);
+      });
+  };
+
+  useEffect(() => {
+    getPostsFromDB()
+  }, []);
+
+  const addPost = () => {
+    const newPost = { post: post };
+    fetch('http://localhost:3001/addpost', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newPost)
+
+    }).then(res => res.json()).then((status) => {
+      console.log("Status of adding post: " + status.status)
+      getPostsFromDB();
+    });
+    setPost("");
+  };
+
+  const deletePost = (id) => {
+    console.log('...requested to update post ' + id);
+    fetch(`http://localhost:3001/posts/${id}`, { method: 'DELETE', headers: { 'Content-Type': 'application/json' } })
+      .then(res => res.json()).then((status) => {
+        console.log("Status of deleting post: " + status.status)
+        getPostsFromDB();
+      });
+  };
+
+  const parseDateAndTime = (createdAt) => {
+    const dateTimeObject = new Date(createdAt);
+  
+    let time = dateTimeObject.toLocaleTimeString().split(":");
+    let unit = time[2].slice(3).toLocaleUpperCase();
+  
+    let date = dateTimeObject.toDateString().split(" ").slice(1);
+  
+    let formattedString = `${date[0]} ${date[1]} ${date[2]} - ${time[0]}:${time[1]} ${unit}`;
+  
+    return formattedString;
+  }
+
   return (
     <div className='react-app-component text-center'>
       <div className="container">
@@ -17,21 +67,25 @@ function App() {
               <div className="card-body">
                 <div className="mb-3">
                   <label className="form-label">Enter your post</label>
-                  <textarea className="form-control" id="post-content" rows="3"></textarea>
+                  <textarea className="form-control" id="post-content" rows="3" value={post} onChange={(e) => (setPost(e.target.value))}></textarea>
                   <div className="d-grid gap-2">
-                    <button type="button" className="btn btn-primary mt-2">Post</button>
+                    <button type="button" className="btn btn-primary mt-2" onClick={() => addPost()}>Post</button>
                   </div>
                 </div>
               </div>
             </div>
 
-            <div className="card text-white bg-dark my-3 text-start">
-              <div className="card-body">
-                <h6 className="card-subtitle mb-2 text-muted">Oct 4, 2022 - 6:15 PM</h6>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="card-link">Delete</a>
+            {posts.map((post, index) => {
+            return (
+              <div key={index} className="card text-white bg-dark my-3 text-start">
+                <div className="card-body">
+                  <h6 className="card-subtitle mb-2 text-muted">{parseDateAndTime(post.createdAt)}</h6>
+                  <p className="card-text">{post.post}</p>
+                  <a href="#" className="card-link" onClick={() => deletePost(post._id)}>Delete</a>
+                </div>
               </div>
-            </div>
+            )
+          })}
           </div>
         </div>
       </div>
